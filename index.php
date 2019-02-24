0e1e63099202ccafa6345e598d0fb988b723157b
shayer <shayerh9@gmail.com>
2019-02-25 01:28:57 +0600

    first upload

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<?php include_once 'templating/head.php';?>
+  <!-- Navigation -->
+  <?php include_once 'templating/navigation.php';?>
+
+  <!-- Page Content -->
+  <!--?php include_once 'templating/side.php';?-->
+      <!-- /.col-lg-3 -->
+
+<!--?php include_once 'templating/element.php';?-->
+
+      <!-- /.col-lg-9 -->
+
+    </div>
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <?php include_once 'templating/footer.php';?>
